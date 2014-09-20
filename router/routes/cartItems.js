907bb13c75e21d03b3169c020516b2a55429dabb
zzh <xiaoyanzhuzzh@sina.com>
2014-09-20 09:06:51 +0800

    add cartItems data

---
+++ router/routes/cartItems.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+var redis = require('redis');
+var client = redis.createClient();
+
+router.get('/', function (req, res) {
+
+  client.get('cartItems', function (err, obj) {
+    res.send(obj);
+  });
+
+});
+
+router.post('/', function (req, res) {
+
+  var cartItems = req.param('cartItems');
+
+  client.set('cartItems', cartItems, function (err, obj) {
+    res.send(obj);
+  });
+
+});
+
+module.exports = router;
