94145945118130536f03ecf6b880e0d5d36779f7
zzh <xiaoyanzhuzzh@sina.com>
2014-09-20 09:05:43 +0800

    add categories data

--- router/routes/categories.js
+++ router/routes/categories.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+var redis = require('redis');
+var client = redis.createClient();
+
+var categories = [
+    {id: 1, name: '饮品'},
+    {id: 2, name: '水果'},
+    {id: 3, name: '零食'},
+    {id: 4, name: '生活用品'}
+  ];
+
+client.set('categories',JSON.stringify(categories));
+
 router.get('/', function(req, res) {
   //TODO: Need to implement.
-  res.send('Success!');
+  client.set('categories', function(err, obj){
+    res.send(JSON.parse(obj));
+  });
+});
+
+router.post('/', function(req, res) {
+  var categories = req.param('categories');
+
+  client.set('categories', categories, function (err, obj) {
+    res.send(obj);
+  });
 });
 
 module.exports = router;
