e1f6ee55a143e8f0803a2c58501104cdcfe432e1
zzh <xiaoyanzhuzzh@sina.com>
2014-10-14 00:10:00 +0800

    delete itemId and CategoryId serve

--- router/index.js
+++ router/index.js
@@ -2,7 +2,5 @@ module.exports = function(app) {
   app.use('/api/categories', require('./routes/categories'));
   app.use('/api/items', require('./routes/items'));
   app.use('/api/cartItems', require('./routes/cartItems'));
-  app.use('/api/maxItemId', require('./routes/maxItemId'));
-  app.use('/api/maxCategoryId', require('./routes/maxCategoryId'));
   app.use('/api/payment', require('./routes/payment'));
 };
