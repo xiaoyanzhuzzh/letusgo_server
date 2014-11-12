529d0bc6454ff2f4f2ea11fba8a4b8a173a4a3d3
zzh <xiaoyanzhuzzh@sina.com>
2014-11-13 00:28:44 +0800

    modify data formatation

--- router/routes/items.js
+++ router/routes/items.js
@@ -7,12 +7,12 @@ var redis = require('redis');
 var client = redis.createClient();
 
 var initItems = [
-  {id: 0, barcode:'ITEM000000', name: '可口可乐', unit: '瓶', price:3.00, category:'饮品'},
-  {id: 1, barcode:'ITEM000001', name: '雪碧', unit:'瓶', price:3.00, category:'饮品'},
-  {id: 2, barcode:'ITEM000002', name:'苹果', unit: '斤',price: 5.50, category:'水果'},
-  {id: 3, barcode:'ITEM000003', name: '荔枝', unit:'斤', price:15.00,category:'水果'},
-  {id: 4, barcode:'ITEM000004', name:'电池', unit: '个', price:2.00, category:'生活用品'},
-  {id: 5,barcode:'ITEM000005', name:'方便面', unit:'袋',price: 4.50, category:'零食'}
+  {id: 0, barcode:'ITEM000000', name: '可口可乐', unit: '瓶', price: 3.00, category: '饮品'},
+  {id: 1, barcode:'ITEM000001', name: '雪碧', unit: '瓶', price: 3.00, category: '饮品'},
+  {id: 2, barcode:'ITEM000002', name: '苹果', unit: '斤', price: 5.50, category: '水果'},
+  {id: 3, barcode:'ITEM000003', name: '荔枝', unit: '斤', price: 15.00,category: '水果'},
+  {id: 4, barcode:'ITEM000004', name: '电池', unit: '个', price: 2.00, category: '生活用品'},
+  {id: 5, barcode:'ITEM000005', name: '方便面', unit: '袋', price: 4.50, category: '零食'}
 ];
 
 function getLastItemId(items) {
